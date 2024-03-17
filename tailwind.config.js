afe45b91199a95612623dd68777dcb0d4b2addd2
Hung Hoang <hunghq.dev@gmail.com>
2024-03-17 12:52:02 +0700

    config tailwind cli

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./src/**/*.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins'],
+      },
+    },
+
+  },
+  plugins: [],
+}
+
---
+++ webpack.common.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './js/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: './js/app.js',
+  },
+  devServer: {
+    port: 3000,
+  }
+};
